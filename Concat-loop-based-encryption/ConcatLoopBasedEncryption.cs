f63d57ae391f0871505fba28d4324d82c6613bd1
Famez1 <a.plyakin@sh.dunice.net>
2024-05-21 11:23:34 +0300

    Добавил класс с имплементацией методов

---
+++ Concat-loop-based-encryption/ConcatLoopBasedEncryption.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concat_loop_based_encryption
+{
+    internal class ConcatLoopBasedEncryption
+    {
+        public string Encrypt(string text, int n)
+        {
+            char[] chars = text.ToCharArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] temp = new char[chars.Length];
+
+                for (int j = 0; j < chars.Length; j++)
+                {
+                    temp[j] = chars[(j + chars.Length / 2) % chars.Length];
+                }
+                chars = temp;
+            }
+            return new string(chars);
+        }
+
+        public string Decrypt(string text, int n)
+        {
+            char[] chars = text.ToCharArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] temp = new char[chars.Length];
+
+                for (int j = 0; j < chars.Length; j++)
+                {
+                    temp[(j + chars.Length / 2) % chars.Length] = chars[j];
+                }
+                chars = temp;
+            }
+
+            return new string(chars);
+        }
+    }
+}
+
---
+++ Concat-loop-based-encryption/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+ConcatLoopBasedEncryption concat = new();
+
+Console.WriteLine(concat.Encrypt("012341", 1));
+Console.WriteLine(concat.Decrypt("341012", 2));
+
+class ConcatLoopBasedEncryption
+{
+    public string Encrypt(string text, int n)
+    {
+        char[] chars = text.ToCharArray();
+
+        for (int i = 0; i < n; i++)
+        {
+            char[] temp = new char[chars.Length];
+
+            for (int j = 0; j < chars.Length; j++)
+            {
+                temp[j] = chars[(j + chars.Length / 2) % chars.Length];
+            }
+            chars = temp;
+        }
+        return new string(chars);
+    }
+
+    public string Decrypt(string text, int n)
+    {
+        char[] chars = text.ToCharArray();
+
+        for (int i = 0; i < n; i++)
+        {
+            char[] temp = new char[chars.Length];
+
+            for (int j = 0; j < chars.Length; j++)
+            {
+                temp[(j + chars.Length / 2) % chars.Length] = chars[j];
+            }
+            chars = temp;
+        }
+
+        return new string(chars);
+    }
+}
+
+
